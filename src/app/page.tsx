abcf3422af9384fb936696f2a47a93ec0fa3190f
eeennsu <diso592@naver.com>
2023-12-03 22:26:29 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const HomePage: NextPage = () => {
+
+    return (
+        <main className='break-'>
+            HomePage
+        </main>
+    );
+};
+
+export default HomePage;
